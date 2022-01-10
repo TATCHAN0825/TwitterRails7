c524d2b08860fc19eb7074e952c2d220e1ffd063
tatchan <43107628+TATCHAN0825@users.noreply.github.com>
2022-01-11 07:38:44 +0900

    rails-i18nを入れる

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,8 @@ module TwitterRails7
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    I18n.config.available_locales = [:ja, :en]
+    I18n.config.default_locale = :ja
   end
 end
